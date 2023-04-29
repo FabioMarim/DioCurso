bc10b2f031baea6ee824fe154bcbf4ac6c625ffb
Fabio Marim <124282188+FabioMarim@users.noreply.github.com>
2023-04-29 01:07:00 -0300

    Add files via upload

---
+++ desafio.rb
@@ -0,0 +1,10 @@
+#Aluno: Fabio Oliveira Marim
+
+print "digite o seu nome completo:"
+name = gets.chomp 
+ 
+print "digite a sua idade:"
+idade = gets.chomp
+
+puts "O seu nome é: #{name}, e sua idade é de: #{idade}"
+
